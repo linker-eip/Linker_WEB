e514107c65031515d17c6b5470ea4bab0b54294e
Alexandre Tourral <alexandre.tourral@epitech.eu>
2023-12-23 14:04:38 +0900

    fix: finish

--- src/Student/Dashbord/Group/partials/InvitationCard.tsx
+++ src/Student/Dashbord/Group/partials/InvitationCard.tsx
@@ -59,6 +59,7 @@ const InvitationCard = (props: Props): JSX.Element => {
         </div>
         <div className='grp-invite-card__content'>
           <div>
+          {/* changer la description la team le type de GroupeInvitation */}
           Description tah les ouf ecrit en dur tah les zhomme SUII aller le football la j aime le foot alesxandDescription tah les ouf ecrit en dur tah les zhomme SUII aller le football la j aime le foot alesxandDescription tah les ouf ecrit en dur tah les zhomme SUII aller le football la j aime le foot alesxandDescription tah les ouf ecrit en dur tah les zhomme SUII aller le football la j aime le foot alesxandDescription tah les ouf ecrit en dur tah les zhomme SUII aller le football la j aime le foot ale
           </div>
           <div className='grp-invite-card__button-section'>
