c451ba75bd3277c8b97cc73859201d87b124d1aa
Prime <theo.tarallo@epitech.eu>
2024-04-17 01:08:55 -0700

    Add: Student incomes connection.

--- src/Student/Statistics/partials/StatisticsSales.tsx
+++ src/Student/Statistics/partials/StatisticsSales.tsx
@@ -1,13 +1,30 @@
-import React, { useState } from 'react'
-import '../../../CSS/StudentDashboardContent.scss'
+/* eslint-disable */
+import React, { useState, useEffect } from 'react'
 import '../../../CSS/StatisticsSales.scss'
 import { useTranslation } from 'react-i18next'
+import ProfileApi from '../../../API/ProfileApi'
+import '../../../CSS/StudentDashboardContent.scss'
 import Graphics from '../../Dashbord/Partials/Graphics'
+import type { StudentStatisticsResponse, Income } from '../../../Typage/ProfileType'
 
 function StatisticsSales (): JSX.Element {
   const { t } = useTranslation()
-  const [salesData] = useState<{ ca: number, nbrMission: number }>({ ca: 880, nbrMission: 2 })
-  const revenueData = {
+
+  const [statistics, setStatistics] = useState<StudentStatisticsResponse | null>(null)
+  
+  useEffect(() => {
+    const fetchData = async (): Promise<StudentStatisticsResponse> => {
+      const studentStatistics = await ProfileApi.getStudentStatistics(localStorage.getItem('jwtToken') as string)
+      setStatistics(studentStatistics)
+      if (Array.isArray(studentStatistics.incomes) && studentStatistics.incomes.length > 0) {
+        updateRevenueData(studentStatistics.incomes)
+      }
+      return studentStatistics
+    }
+    fetchData()
+  }, [])
+
+  const [revenueData, setRevenueData] = useState({
     labels: [
       'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin',
       'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'
@@ -15,13 +32,25 @@ function StatisticsSales (): JSX.Element {
     datasets: [
       {
         label: 'Chiffre d\'affaires',
-        data: [0, 0, 0, 0, 0, 0, 440, 440, 440, 440, 880, 880],
+        data: Array(12).fill(0),
         fill: true,
         backgroundColor: 'rgba(75, 192, 192, 0.2)',
         borderColor: 'rgba(75, 192, 192, 1)',
         borderWidth: 2
       }
     ]
+  })
+
+  const updateRevenueData = (incomes: Income[]): void => {
+    const newRevenueData = revenueData.datasets[0].data.slice()
+    incomes.forEach((income: Income) => {
+      const month = new Date(income.paymentDate).getMonth()
+      newRevenueData[month] += income.amount
+    })
+    setRevenueData(prevState => ({
+      ...prevState,
+      datasets: [{ ...prevState.datasets[0], data: newRevenueData }]
+    }))
   }
 
   return (
@@ -33,14 +62,14 @@ function StatisticsSales (): JSX.Element {
             <img className='stats-sales__logo' src='/assets/wallet.svg' />
             <div className='stats-sales__sub-section'>
               <p className='stats-sales__text-1'> {t('student.dashboard.card.sales.sales')} </p>
-              <p> { salesData.ca } € </p>
+              <p> { statistics?.incomes?.reduce((acc, income) => acc + income.amount, 0) } € </p>
             </div>
           </div>
           <div className='stats-sales__content'>
             <img className='stats-sales__logo' src='/assets/validate.svg' />
             <div className='stats-sales__sub-section'>
               <p className='stats-sales__text-1'> {t('student.dashboard.card.sales.finished_missions')} </p>
-              <p> { salesData.nbrMission } </p>
+              <p> { statistics?.incomes?.length } </p>
             </div>
           </div>
         </div>
