6ffd60207f70d1ad879b80deb276a1fce048d19b
Alexandre Tourral <alexandre.tourral@epitech.eu>
2024-04-28 15:08:53 +0900

    fix: merged issues

--- src/Company/Partials/NotificationCard.tsx
+++ src/Company/Partials/NotificationCard.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 
 import React from 'react'
-import '../../../CSS/NotificationCard.scss'
-import type { Notifications } from '../../../Typage/NotificationType'
+import '../../CSS/NotificationCard.scss'
+import type { Notifications } from '../../Typage/NotificationType'
 import moment from 'moment'
 import 'moment/locale/fr'
-import NotificationApi from '../../../API/NotificationApi'
-import { NotificationType } from '../../../Enum'
-import * as ROUTES from '../../../Router/routes'
+import NotificationApi from '../../API/NotificationApi'
+import { NotificationType } from '../../Enum'
+import * as ROUTES from '../../Router/routes'
 
 import CloseIcon from '@mui/icons-material/Close'
 import { useNavigate } from 'react-router-dom'
@@ -18,7 +18,7 @@ interface Props {
   onReload: () => void
 }
 
-function NotificationCard(props: Props): JSX.Element {
+function NotificationCard (props: Props): JSX.Element {
   const navigate = useNavigate()
   moment.locale('fr')
   const date = moment(props.data.date).format('DD MMMM YYYY')
@@ -29,17 +29,14 @@ function NotificationCard(props: Props): JSX.Element {
   }
 
   const redirectNotification = (): void => {
-    if (props.data.type === NotificationType.GROUP) {
-      navigate(ROUTES.STUDENT_GROUP)
-    }
     if (props.data.type === NotificationType.MISSION) {
-      navigate(ROUTES.STUDENT_MISSIONS)
+      navigate(ROUTES.COMPANY_MISSIONS)
     }
     if (props.data.type === NotificationType.DOCUMENT) {
-      navigate(ROUTES.STUDENT_DOCUMENTS_DASHBOARD)
+      navigate(ROUTES.COMPANY_DOCUMENTS_DASHBOARD)
     }
     if (props.data.type === NotificationType.MESSAGE) {
-      navigate(ROUTES.STUDENT_GROUP)
+      navigate(ROUTES.COMPANY_MISSIONS)
     }
   }
 
