efd8ce6eb6f857696ea55957b96b1639d8ffce1e
Alexandre Tourral <alexandre.tourral@epitech.eu>
2024-05-09 09:23:27 +0900

    fix: User Verification Accout

--- src/Component/isPrivateRoute.ts
+++ src/Component/isPrivateRoute.ts
@@ -1,10 +1,24 @@
 import { useNavigate } from 'react-router-dom'
 import * as ROUTES from '../Router/routes'
 import CheckAuth from './CheckAuth'
+import { useEffect, useState } from 'react'
+import AuthApi from '../API/AuthApi'
 
 function isPrivateRoute (): void {
+  const [isVerified, setIsVerified] = useState<boolean>(true)
+
+  useEffect(() => {
+    const fetchData = async (): Promise<any> => {
+      const response = await AuthApi.VerifyStudentAccount(localStorage.getItem('jwtToken') as string)
+      if (response !== undefined) {
+        setIsVerified(response)
+      }
+    }
+    fetchData()
+  }, [])
+
   const navigate = useNavigate()
-  if (!CheckAuth()) {
+  if (!CheckAuth() && !isVerified) {
     navigate(ROUTES.LANDING_PAGE)
   }
 }
