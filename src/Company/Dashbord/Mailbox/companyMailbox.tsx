d0e6b037c89d81b605eb921ee3b817bc863de9a7
Théo Tarallo <theo.tarallo@epitech.eu>
2024-09-12 02:13:06 +0200

    [Add] - Mailbox tab sidebar, missing content.

---
+++ src/Company/Dashbord/Mailbox/companyMailbox.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../../../CSS/StudentDashboard.scss'
+import HotbarDashboard from '../../Partials/HotbarDashboard'
+import SidebarDashboard from '../../Partials/SidebarDashboard'
+import { useTranslation } from 'react-i18next'
+import { DashboardState } from '../../../Enum'
+import CompanyMailboxContent from './Partials/companyMailboxContent'
+
+function CompanyMailbox (): JSX.Element {
+  const state = DashboardState
+  const { t } = useTranslation()
+
+  return (
+    <div className="std-bord-container">
+      <HotbarDashboard> {t('student.dashboard.mailbox')} </HotbarDashboard>
+      <div className="std-bord-container__page">
+        <SidebarDashboard state={state.MAILBOX} />
+        <div className="std-bord-container__content">
+          <CompanyMailboxContent />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CompanyMailbox
