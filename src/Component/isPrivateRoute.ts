f0c7577ff7a0068c3683f8a7da76f2e3f50afe4b
Alexandre tourral <72022812+AlexandreTourral@users.noreply.github.com>
2024-10-20 12:02:13 +0200

    fix meta donnée (#128)
    Co-authored-by: Alexandre Tourral <alexandretourral@MacBook-Pro-de-Alexandre.local>

--- src/Component/isPrivateRoute.ts
+++ src/Component/isPrivateRoute.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { useNavigate } from 'react-router-dom'
 import * as ROUTES from '../Router/routes'
 import CheckAuth, { CheckAdmin } from './CheckAuth'
